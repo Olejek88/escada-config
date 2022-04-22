f170ea0e1142b95ffd9963ebc8240c8de01a93aa
Dmitriy Logachov <demonwork@yandex.ru>
2022-04-22 13:19:55 +0500

    Параметры по умолчанию для консольной конфигурации.

--- environments/dev/console/config/main-local.php
+++ environments/dev/console/config/main-local.php
@@ -4,4 +4,23 @@ return [
     'modules' => [
         'gii' => 'yii\gii\Module',
     ],
+    'mtm-worker' => [
+        // контроллер для запуска "демона" console\workers\MtmAmqpWorker в докере
+        'class' => 'console\controllers\MtmWorker',
+        'logFile' => 'php://stdout',
+    ],
+    'daemon' => [
+        'class' => 'inpassor\daemon\Controller',
+        'uid' => 'daemon',
+        'pidDir' => '@console/runtime/daemon',
+        'logsDir' => '@console/runtime/daemon/logs',
+        'clearLogs' => false,
+        'workersMap' => [
+            'mtm_amqp_worker' => [
+                'class' => 'console\workers\MtmAmqpWorker',
+                'active' => true,
+                'maxProcesses' => 1,
+            ],
+        ],
+    ],
 ];
